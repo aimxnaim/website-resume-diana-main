9cb9467f1a19ee5d58bab28d33429dd772df1d47
Aiman Naim <76118353+aimxnaim@users.noreply.github.com>
2024-10-07 23:32:00 +0800

    first commit

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+import styles from "./Hero.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Hero = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I'm Diana</h1>
+        <p className={styles.description}>
+          I'm a recent graduate with a degree in Information Systems Engineering, passionate about systems and related fields. Reach out if you'd like to learn more!
+        </p>
+        <a href="mailto:myemail@email.com" className={styles.contactBtn}>
+          Contact Me
+        </a>
+      </div>
+      <img
+        src={getImageUrl("hero/heroImage.png")}
+        alt="Hero image of me"
+        className={styles.heroImg}
+      />
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
+    </section>
+  );
+};
